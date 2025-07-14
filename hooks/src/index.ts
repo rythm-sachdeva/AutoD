e3fccdf96b04c595cb9a397f5ca9bcef0232ef35
rythm-sachdeva <rythmrko123@gmail.com>
2025-07-14 20:25:48 +0530

    Created Transactions

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,35 @@
 import express from 'express';
 
 const app = express();
+import { PrismaClient } from './generated/prisma';
+
+const client = new PrismaClient();
 
 
-app.post('/hooks/catch/:userId/:zapId',(req,res)=>{
+app.post('/hooks/catch/:userId/:zapId', async (req,res)=>{
     const { userId, zapId } = req.params;
+    const body = req.body;
     console.log(`Received hook for user ${userId} and zap ${zapId}`);
     
     // store in db a new trigger
-
-    //push it on to the queue (Kafka/Redis)
+   await client.$transaction(async (tx)=>{
+    const run =  await client.zapRun.create({
+        data:{
+            zapId: zapId
+        }
+    })
+    await client.zapRunOutbox.create({
+        data:{
+            zapRunId: run.id,
+            metadata: body
+        }
+    })
+   })
     
+    //push it on to the queue (Kafka/Redis)
+
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
 })
